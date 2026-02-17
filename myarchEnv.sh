713e7771fdc3c0ed7d6ef37a914519ff2ed62df5
pachhhi <carrizotomasss@outlook.com>
2026-02-17 08:10:08 +0000

    updated

--- myarchEnv.sh
+++ myarchEnv.sh
@@ -25,6 +25,7 @@ fi
 
 
 #Verify connection and Update system
+echo -e "${BLUE} > Verify connection.. ${RESET}"
 systemctl enable NetworkManager
 ping -c 1 8.8.8.8 &> /dev/null
 if [ $? -eq 0 ]; then 
@@ -33,37 +34,73 @@ else
 	echo -e "${RED} You are offline... :( ${RESET}" exit 1;
 fi
 
-sudo pacman -Syu --noconfirm
+#sudo pacman -Syu --noconfirm
 
 #Make config dir
 echo -e "${GREEN}λ > Creating config dir in ${USER_HOME} ${RESET}"
-mkdir -p "$USER_HOME/myarchEnvconfig"
+#mkdir -p "$USER_HOME/myarchEnvconfig"
 
 #Installing drivers for GPU
-pacman -S --noconfirm x86-video-amdgpu mesa vulkan-radeon libva-mesa-driver
+#pacman -S --noconfirm x86-video-amdgpu mesa vulkan-radeon libva-mesa-driver
 
 #Download packages for BSPWM
 function configBSPWM() {
-	pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot bspwm sxhkd picom rofi kitty zsh feh lightdm lightdm-gtk-greeter
-	mkdir -p "$USER_HOME/myarchEnvconfig/.config/{bspwm,sxhkd,picom,kitty,rofi}"
+	
+#	pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot bspwm sxhkd picom rofi kitty zsh feh lightdm lightdm-gtk-greeter
+#	mkdir -p "$USER_HOME/myarchEnvconfig/.config/{bspwm,sxhkd,picom,kitty,rofi}"	
+	echo -e "${BLUE} > Making config for bspwm ${RESET}"
 }
+configBSPWM
 
 #Download packages for HYPRLAND
-pacman -S --noconfirm hyprland waybar wofi xdg-desktop-portal-hyprland wayland wayland-protocols xwayland sddm
-systemctl enable sddm
+#pacman -S --noconfirm hyprland waybar wofi xdg-desktop-portal-hyprland wayland wayland-protocols xwayland sddm
+#systemctl enable sddm
 
 #Download packages for KDE
-pacman -S --noconfirm plasma kde-applications
+#pacman -S --noconfirm plasma kde-applications
 
 #Functions
 
 function installAUR() {
-	echo -e "${GREEN}λ > Installing AUR in ${USER_HOME} ${RESET}"
+	echo -e "${BLUE}λ > Installing AUR in ${USER_HOME} ${RESET}"
 	#sudo pacman -S --needed base-devel git
 	#git clone https://aur.archlinux.org/yay.git $USER_HOME/myarchEnvconfig
+	#cd $USER_HOME/myarchEnvconfig/yay/
+	#makepkg -si
+	echo -e "${GREEN} > Installed! ${RESET}"
 
-	
 }
 
+function initInstall() {
+	echo -e "${GREEN} >Do you want make a custom install? (default/custom)${RESET}"
+	read choice
+
+	case $choice in
+		default)
+			echo -e "${GREEN} > Preparing install..${RESET}"
+			
+			configBSPWM
+			wait
+
+			installAUR
+			wait
 
 
+			;;
+		custom)
+			echo -e "${GREEN} > Preparing custom install.. ${RESET}"
+			
+			echo -e "${GREEN} > Do you want install BSPWM? ${RESET}"
+			read bspwm
+			if [ $bspwm == y ]; then
+				echo -e "${GREEN} > Installing BSPWM.. ${RESET}"
+				#configBSPWM
+			fi
+			;;
+		*)
+			echo -e "${RED} > Choice (default) or (custom) ${RESET}"
+			;;
+	esac
+}
+
+initInstall
